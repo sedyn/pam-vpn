d770f57eca25fc62ebaab9df6a964e6880e3bbbb
Sehyun Park <doodot.dev@gmail.com>
2023-08-08 07:05:02 -0700

    add credential input

--- src/vpn/credential.rs
+++ src/vpn/credential.rs
@@ -1,4 +1,17 @@
+use std::io::Write;
+
 pub struct UserPasswordCredential<'a> {
     pub user: &'a str,
-    pub password: &'a str
+    pub password: &'a str,
+}
+
+impl<'a> UserPasswordCredential<'a> {
+    pub fn write<T: Write>(&self, writer: &mut T) -> anyhow::Result<()> {
+        writer.write(self.user.as_bytes())?;
+        writer.flush()?;
+        writer.write(self.password.as_bytes())?;
+        writer.flush()?;
+
+        Ok(())
+    }
 }
--- src/vpn/openvpn.rs
+++ src/vpn/openvpn.rs
@@ -1,4 +1,11 @@
-use std::process::Command;
+use std::{
+    env::temp_dir,
+    fs::{remove_file, File},
+    io::Write,
+    process::Command,
+};
+
+use anyhow::Context;
 
 use super::{credential::UserPasswordCredential, VPNClient};
 
@@ -6,12 +13,18 @@ pub struct OpenVPNClient;
 
 impl VPNClient<UserPasswordCredential<'_>> for OpenVPNClient {
     fn connect(&self, credential: UserPasswordCredential) -> anyhow::Result<()> {
-        let output = Command::new("openvpn")
-            .arg("--username ")
-            .arg(credential.user)
-            .arg("--password")
-            .arg(credential.password)
-            .output()?;
+        let mut child = Command::new("openvpn")
+            .arg("--config")
+            .arg("client.conf")
+            .arg("--auth-user-pass")
+            .spawn()?;
+
+        let stdin = child
+            .stdin
+            .as_mut()
+            .context("unable to acquire stdin of child")?;
+
+        credential.write(stdin)?;
 
         Ok(())
     }
